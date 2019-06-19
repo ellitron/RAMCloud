753333ec77a1e809a9dc617c51f826b91df5966c
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-06-18 20:01:28 -0600

    Changed read only tx to not register on server.
    Read only transactions no longer register with the TransactionManager in
    MasterService::txPrepare(). This was leading to unnecessary transaction timeouts
    because read only transactions are not committed by the client after the prepare
    phase. While this did not affect correctness, it was resulting in unnecessary
    work for the server, especially if a client executes a long stream of read-only
    transactions.

--- src/MasterService.cc
+++ src/MasterService.cc
@@ -2874,19 +2874,26 @@ MasterService::txPrepare(const WireFormat::TxPrepare::Request* reqHdr,
     // Ensure the soon to be registered transaction is not garbage collected
     // before this transaction prepare request is processed.
     TransactionManager::Protector protectTransaction(&transactionManager, txId);
+    
+    // 2. Process operations.
+    uint32_t numRequests = reqHdr->opCount;
+    uint32_t numReadOnly = 0;
 
-    if (transactionManager.registerTransaction(participantList,
+    assert(numRequests > 0);
+
+    const WireFormat::TxPrepare::OpType *type =
+            rpc->requestPayload->getOffset<
+            WireFormat::TxPrepare::OpType>(reqOffset);
+
+    if ((*type != WireFormat::TxPrepare::READONLY) && 
+        (transactionManager.registerTransaction(participantList,
                                                assembledParticpantList,
                                                objectManager.getLog())
-            != STATUS_OK) {
+            != STATUS_OK)) {
         respHdr->common.status = STATUS_RETRY;
         rpc->sendReply();
         return;
     }
-
-    // 2. Process operations.
-    uint32_t numRequests = reqHdr->opCount;
-    uint32_t numReadOnly = 0;
 
     clusterClock.updateClock(ClusterTime(reqHdr->lease.timestamp));
 
